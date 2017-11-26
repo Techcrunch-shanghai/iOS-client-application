c7ba5c2729223da3b97b23392321da14728637f1
r é m i  <remi.robert@epitech.eu>
2017-11-26 08:59:32 +0800

    add local json to the map service

--- App/Fake/FakeMapService.swift
+++ App/Fake/FakeMapService.swift
@@ -15,6 +15,20 @@ class FakeMapService: MapServiceProvider {
     }
 
     func fetchPoints(completion: @escaping ([MapAnnotation]) -> Void) {
-        completion(points)
+        guard let path = Bundle.main.path(forResource: "merchant", ofType: "json") else { return }
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+            if let jsonResult = jsonResult as? Dictionary<String, AnyObject> {
+                let merchants = Merchant.create(fromJSON: jsonResult)
+                let points = merchants.map({
+                    return MapAnnotation(merchant: $0)
+                })
+                completion(points)
+            }
+            completion([])
+        } catch {
+            completion([])
+        }
     }
 }
--- App/Map/MapView/MapAnnotation.swift
+++ App/Map/MapView/MapAnnotation.swift
@@ -8,15 +8,41 @@
 
 import MapKit
 
+struct Merchant {
+    let title: String
+    let latitude: Double
+    let longitude: Double
+    let imageUrl: String?
+
+    init(json: [String:Any]) {
+        title = json["name"] as? String ?? ""
+        if let location = json["location"] as? [String:Double] {
+            latitude = location["lat"] ?? 0
+            longitude = location["lng"] ?? 0
+        } else {
+            latitude = 0
+            longitude = 0
+        }
+        imageUrl = json[""] as? String
+    }
+
+    static func create(fromJSON json: [String:Any]) -> [Merchant] {
+        return ((json["merchantList"] as? [[String:Any]]) ?? []).map {
+            return Merchant(json: $0)
+        }
+    }
+}
+
 class MapAnnotation: NSObject, MKAnnotation {
     let title: String? = nil
     let subtitle: String? = nil
     let coordinate: CLLocationCoordinate2D
-    let index: Int
+    let merchant: Merchant
 
-    init(point: DataPoint, index: Int) {
-        coordinate = CLLocationCoordinate2D(latitude: point.lat, longitude: point.long)
-        self.index = index
+    init(merchant: Merchant) {
+        self.merchant = merchant
+        coordinate = CLLocationCoordinate2D(latitude: merchant.latitude,
+                                            longitude: merchant.longitude)
         super.init()
     }
 }
